cfacdee498faae08ccbe37a2341bd8648b048318
solitas.roh <solitasroh@gmail.com>
2021-02-06 15:50:59 +0900

    POC API 적용

--- server.js
+++ server.js
@@ -2,14 +2,28 @@ import { gql, ApolloServer } from "apollo-server";
 
 // The graphQL scheme
 const typeDefs = gql`
+    type Movie {
+        title: String
+        year: Int
+    }
     type Query {
-        hello: String
+        movies: [Movie]
+        movie: Movie
+    }
+    type Mutation {
+        createMovie(title:String!): Boolean
+        removeMovie(title:String!): Boolean
     }
 `;
 
 const resolvers = {
     Query: {
-        hello: () => 'world',
+        movies: () => [],
+        movie: () => ({ title: "Hello", year: 2021 }),
+    },
+    Mutation: {
+        createMovie: (_, args) => true,
+        removeMovie: (_, args) => true,
     }
 };
 
