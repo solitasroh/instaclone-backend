09da121d84e3a0f8df062a01ef7ad179801effbe
soojang <solitasroh@gmail.com>
2021-03-13 10:08:38 +0900

    언팔로우 유저 구현

---
+++ users/unfollowUser/unfollowUser.resolvers.js
@@ -0,0 +1,24 @@
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+export default {
+    Mutation: {
+        unfollowUser: protectedResolver( async (_, {userName}, {loggedInUser}) => {
+            const ok = await client.user.findUnique({ where: { userName }});
+            if (!ok) {
+                return {
+                    ok: false,
+                    error: "That user does not exist.",
+                }
+            }
+
+            await client.user.update({
+                where: {id: loggedInUser.id },
+                data: { following: { disconnect: { userName }}},
+            });
+            return  {
+                ok: true,
+            }
+        }),
+    }
+};
