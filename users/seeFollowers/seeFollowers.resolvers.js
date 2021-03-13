32f52f123f200f83244bf5b301e8934d0e9dcadb
soojang <solitasroh@gmail.com>
2021-03-13 16:25:19 +0900

    팔로잉 기능 구현

--- users/seeFollowers/seeFollowers.resolvers.js
+++ users/seeFollowers/seeFollowers.resolvers.js
@@ -5,6 +5,17 @@ export default {
     Query: {
         
         seeFollowers: async (_, {userName, page}) => {
+            const ok = await client.user.findUnique({
+                where: { userName },
+                select: { id: true}
+            });
+            
+            if (!ok) {
+                return {
+                    ok: false,
+                    error: "That user does not exist.",
+                };
+            }
             const followers = await client.user
                         .findUnique({ where: { userName } })
                         .followers({take: 5, skip: (page-1) * pageCount});
