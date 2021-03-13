ffd023d597cf28fb686e63e03b499e90541902e3
soojang <solitasroh@gmail.com>
2021-03-13 17:04:56 +0900

    computed field 적용 (user)

---
+++ users/users.resolvers.js
@@ -0,0 +1,37 @@
+import client from "../client"
+import { protectedResolver } from "./users.utils"
+
+export default {
+    User: {
+        totalFollowing: async ({id}) => {
+            return await client.user.count({
+                where: {followers: {
+                    some: { id }
+                }}
+            })
+        },
+        totalFollowers: async ({id}) => {
+            return await client.user.count({
+                where: {following: {
+                    some: { id }
+                }}
+            })
+        },
+        isMe: async ({id}, _, {loggedInUser}) => {
+            if (!loggedInUser) return false;
+            return id === loggedInUser.id;
+        },
+        isFollowing: async ({id}, _,  {loggedInUser}) => {
+            if (!loggedInUser) return false;
+            // const exists = await client.user
+            //     .findUnique({ where: {id: loggedInUser.id}})
+            //     .following( { where: { id, } });
+            const exists = await client.user.count({
+                where: { userName: loggedInUser.userName},
+                following: { some: {id}}
+            });
+            return Boolean(exists);
+        },
+    }
+
+}
