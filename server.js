104cf86f6075b9c102c967ef8346653e317eac62
solitasroh <solitasroh@gmail.com>
2021-04-14 21:34:09 +0900

    create account 수정

--- server.js
+++ server.js
@@ -1,56 +1,54 @@
-import dotenv from 'dotenv'
-import express from 'express'
-import logger from 'morgan'
-import { getUser, protectResolver } from './users/users.utils'
-dotenv.config()
-import http from 'http'
-import { ApolloServer } from 'apollo-server-express'
-import { typeDefs, resolvers } from './schema'
-import { graphqlUploadExpress } from 'graphql-upload'
+import dotenv from 'dotenv';
+import express from 'express';
+import logger from 'morgan';
+import { getUser, protectResolver } from './users/users.utils';
+dotenv.config();
+import http from 'http';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs, resolvers } from './schema';
+import { graphqlUploadExpress } from 'graphql-upload';
 
 // The graphQL scheme
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 const apollo = new ApolloServer({
-    resolvers,
-    typeDefs,
-    uploads: false,
-    context: async ({ ctx }) => {
-        if (ctx) {
-            return {
-                loggedInUser: await getUser(ctx.req.headers.token),
-                protectResolver,
-            }
-        } else {
-            const {
-                connection: { context },
-            } = ctx
-            return {
-                loggedInUser: context.loggedInUser,
-            }
-        }
-    },
-    subscriptions: {
-        onConnect: async ({ token }) => {
-            if (!token) {
-                throw new Error("you can't listen.")
-            }
-            const loggedInUser = await getUser(token)
-            return {
-                loggedInUser,
-            }
-        },
-    },
-})
+	resolvers,
+	typeDefs,
+	uploads: false,
+	context: async (ctx) => {
+		if (ctx) {
+			return {
+				loggedInUser: await getUser(ctx.req.headers.token)
+			};
+		} else {
+			const { connection: { context } } = ctx;
+			return {
+				loggedInUser: context.loggedInUser
+			};
+		}
+	},
+	subscriptions: {
+		onConnect: async ({ token }) => {
+			if (!token) {
+				throw new Error("you can't listen.");
+			}
+			const loggedInUser = await getUser(token);
+			return {
+				loggedInUser
+			};
+		}
+	},
+	introspection: true
+});
 
-const app = express()
-app.use(logger('tiny'))
-app.use(graphqlUploadExpress())
-apollo.applyMiddleware({ app })
+const app = express();
+app.use(logger('tiny'));
+app.use(graphqlUploadExpress());
+apollo.applyMiddleware({ app });
 
-app.use('/static', express.static('uploads'))
+app.use('/static', express.static('uploads'));
 
-const httpServer = http.createServer(app)
-apollo.installSubscriptionHandlers(httpServer)
+const httpServer = http.createServer(app);
+apollo.installSubscriptionHandlers(httpServer);
 httpServer.listen(PORT, () => {
-    console.log(`✅ Server ready at http://localhost:${PORT}`)
-})
+	console.log(`✅ Server ready at http://localhost:${PORT}`);
+});
--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -1,45 +1,48 @@
-import bycrpty from 'bcrypt'
-import client from '../../client'
+import bycrpty from 'bcrypt';
+import client from '../../client';
 
 export default {
-    Mutation: {
-        createAccount: async (
-            _,
-            { firstName, lastName, userName, email, password }
-        ) => {
-            // check if username or email are already on DB.
-            try {
-                const existingUser = await client.user.findFirst({
-                    where: {
-                        OR: [
-                            {
-                                userName,
-                            },
-                            {
-                                email,
-                            },
-                        ],
-                    },
-                })
+	Mutation: {
+		createAccount: async (_, { firstName, lastName, userName, email, password }) => {
+			// check if username or email are already on DB.
+			try {
+				const existingUser = await client.user.findFirst({
+					where: {
+						OR: [
+							{
+								userName
+							},
+							{
+								email
+							}
+						]
+					}
+				});
 
-                if (existingUser) {
-                    throw new Error('This username/email is already taken')
-                }
+				if (existingUser) {
+					throw new Error('This username/email is already taken');
+				}
 
-                const uglyPassword = await bycrpty.hash(password, 10)
-                // save and return the user
-                return client.user.create({
-                    data: {
-                        firstName,
-                        lastName,
-                        userName,
-                        email,
-                        password: uglyPassword,
-                    },
-                })
-            } catch (error) {
-                return error
-            }
-        },
-    },
-}
+				const uglyPassword = await bycrpty.hash(password, 10);
+				// save and return the user
+				await client.user.create({
+					data: {
+						firstName,
+						lastName,
+						userName,
+						email,
+						password: uglyPassword
+					}
+				});
+				return {
+					ok: true
+				};
+			} catch (error) {
+				return {
+					ok: false,
+					error: 'cant create account'
+				};
+			}
+		}
+	}
+};
