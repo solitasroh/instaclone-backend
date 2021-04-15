f6520570c9fddea92efa1a91458e332900782f5b
solitasroh <solitasroh@gmail.com>
2021-04-15 23:02:47 +0900

    lsLiked 추가

--- photos/photos.resolvers.js
+++ photos/photos.resolvers.js
@@ -1,53 +1,71 @@
-import client from '../client'
-let pageCount = 5
+import client from '../client';
+let pageCount = 5;
 export default {
-    Photo: {
-        user: ({ userId }) => {
-            console.log(userId)
-            return client.user.findUnique({ where: { id: userId } })
-        },
-        hashtags: ({ id }) => {
-            return client.hashtag.findMany({
-                where: {
-                    photos: {
-                        some: {
-                            id,
-                        },
-                    },
-                },
-            })
-        },
-        likes: ({ id }) => client.like.count({ where: { photoId: id } }),
-        comments: ({ id }) => client.comment.count({ where: { photoId: id } }),
-        isMine: ({ userId }, _, { loggedInUser }) => {
-            if (!loggedInUser) return false
-            return userId == loggedInUser.id
-        },
-    },
-    Hashtag: {
-        totalPhotos: ({ id }) => {
-            return client.photo.count({
-                where: {
-                    hashtags: {
-                        some: {
-                            id,
-                        },
-                    },
-                },
-            })
-        },
-        photos: ({ id }, { page }) => {
-            return client.photo.findMany({
-                where: {
-                    hashtags: {
-                        some: {
-                            id,
-                        },
-                    },
-                },
-                take: 5,
-                skip: (page - 1) * pageCount,
-            })
-        },
-    },
-}
+	Photo: {
+		user: ({ userId }) => {
+			console.log(userId);
+			return client.user.findUnique({ where: { id: userId } });
+		},
+		hashtags: ({ id }) => {
+			return client.hashtag.findMany({
+				where: {
+					photos: {
+						some: {
+							id
+						}
+					}
+				}
+			});
+		},
+		likes: ({ id }) => client.like.count({ where: { photoId: id } }),
+		comments: ({ id }) => client.comment.count({ where: { photoId: id } }),
+		isMine: ({ userId }, _, { loggedInUser }) => {
+			if (!loggedInUser) return false;
+			return userId == loggedInUser.id;
+		},
+		isLiked: async ({ id }, _, { loggedInUser }) => {
+			if (!loggedInUser) return false;
+			const ok = await client.like.findUnique({
+				where: {
+					photoId_userId: {
+						photoId: id,
+						userId: loggedInUser.id
+					}
+				},
+				select: {
+					id: true
+				}
+			});
+			if (ok) {
+				return true;
+			}
+			return false;
+		}
+	},
+	Hashtag: {
+		totalPhotos: ({ id }) => {
+			return client.photo.count({
+				where: {
+					hashtags: {
+						some: {
+							id
+						}
+					}
+				}
+			});
+		},
+		photos: ({ id }, { page }) => {
+			return client.photo.findMany({
+				where: {
+					hashtags: {
+						some: {
+							id
+						}
+					}
+				},
+				take: 5,
+				skip: (page - 1) * pageCount
+			});
+		}
+	}
+};
